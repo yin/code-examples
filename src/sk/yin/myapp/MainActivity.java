3419e0cbb17f0a82c015384fb6315138f4ab471b
Matej Gagyi <matej.gagyi@gmail.com>
2013-10-04 13:02:02 +0200

    Fixed the toHex() function for HashedString.

--- src/sk/yin/myapp/MainActivity.java
+++ src/sk/yin/myapp/MainActivity.java
@@ -1,22 +1,49 @@
 package sk.yin.myapp;
 
 import android.app.*;
+import android.content.*;
 import android.os.*;
+import android.view.*;
 import android.widget.*;
 import java.security.*;
-import java.util.*;
+import sk.yin.myapp.MainActivity.*;
 
 public class MainActivity extends ListActivity
 {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
-	{
+	{   final int itemId = android.R.layout.two_line_list_item;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 		ListAdapter adapter = new ArrayAdapter<HashedString>(this,
-		   android.R.layout.simple_list_item_1,
-		   new HashedString[] {hs("one"),hs("two")});
+		    itemId,
+		    new HashedString[] {hs("one"),hs("two")}) {
+			    @Override
+			    public View getView(int position, View convertView, ViewGroup parent) {
+					LayoutInflater inflater = (LayoutInflater)getContext()
+				       .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+					View listItemView = convertView;
+					if (null == convertView) {
+						listItemView = inflater.inflate(itemId, parent, false);
+					}
+					// The ListItemLayout must use the standard text item IDs.
+					TextView lineOneView = (TextView)listItemView.findViewById(android.R.id.text1);
+					TextView lineTwoView = (TextView)listItemView.findViewById(android.R.id.text2);
+					HashedString t = (HashedString)getItem(position);
+					lineOneView.setText(lineOneText(t));
+					lineTwoView.setText(lineTwoText(t));
+					return listItemView;
+				}
+
+				private CharSequence lineOneText(HashedString t) {
+					return t.getStr();
+				}
+				
+				private CharSequence lineTwoText(MainActivity.HashedString t) {
+					return t.getSha1();
+				}
+			};
 		setListAdapter(adapter);
 	}
 	
@@ -64,12 +91,16 @@ public class MainActivity extends ListActivity
 			for (byte b : hash) {
 				int u = (b >> 4) & 0x0F,
 				    l = b & 0x0F;
-				sb.append(u<10 ? ('0'+u) : ('a'+u-10))
-				    .append(l<10 ? ('0'+l) : ('a'+l-10));
+				char uhex = toHex(u),
+				    lhex = toHex(l);
+			    sb.append(uhex).append(lhex);
 			}
 			return sb.toString();
 		}
-
+		private char toHex(int i) {
+			return (char) (i < 10 ? '0' + i : 'a' + (i % 10));
+		}
+		
 		public void setStr(String str)
 		{
 			this.str = str;
