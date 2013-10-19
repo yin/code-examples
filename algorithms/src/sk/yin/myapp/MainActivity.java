e737456ad1de4192588e85f2493d6e9aa0350838
Matej 'Yin' Gagyi <yinotaurus@gmail.com>
2013-10-19 10:28:46 +0200

    Moved the algorithms project to own subdirectory.

--- algorithms/src/sk/yin/myapp/MainActivity.java
+++ algorithms/src/sk/yin/myapp/MainActivity.java
@@ -1,13 +1,13 @@
 package sk.yin.myapp;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends Activity {
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
