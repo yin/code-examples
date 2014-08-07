c8141ec78b01664f6a45de4e7b7231c1c14bcf7b
Matej 'Yin' Gagyi <yinotaurus@gmail.com>
2014-08-07 21:46:01 +0200

    FizzBuzz java implementation

---
+++ com/github/yin/java/crafts/FizzBuzz.java
@@ -0,0 +1,42 @@
+package com.github.yin.java.crafts;
+
+import java.lang.NumberFormatException;
+import java.io.PrintStream;
+
+public class FizzBuzz {
+    private static final int DEFAULT_NUM = 20;
+
+    public void fizzbuzz(int num, PrintStream out) {
+        for (int i = 0; i < num + 1; i++) {
+            String s = "";
+            int i3 = i % 3;
+            int i5 = i % 5;
+            if (i3 == 0) {
+                s = "Fizz";
+            }
+            if (i5 == 0) {
+                s += "Buzz";
+            }
+            if (i3 != 0 && i5 != 0) {
+                s = String.valueOf(i);
+            }
+            out.println(s);
+        }
+    }
+
+    public static void main(String args[]) {
+        int num = DEFAULT_NUM;
+        FizzBuzz me = new FizzBuzz();
+        if (args.length > 1) {
+            try {
+                num = Integer.valueOf(args[1]);
+            } catch(NumberFormatException e) {
+                System.err.println(String.format("Next time use: java %s <num>",
+                                                 FizzBuzz.class.getName()));
+                System.err.println(String.format("Defaulting to num = %d",
+                                                 DEFAULT_NUM));
+            }
+        }
+        me.fizzbuzz(num, System.out);
+    }
+}
---
+++ scripts/string-buffer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+javac com/github/yin/java/crafts/FizzBuzz.java && \
+    java com.github.yin.java.crafts.FizzBuzz $1
