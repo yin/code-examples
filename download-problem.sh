ec056876e13079e0c28a777965ff9e1943d7c37b
Matej 'Yin' Gagyi <yinotaurus@gmail.com>
2014-08-30 01:33:43 +0200

    Changed download-problem to generate pdf, compress the html and excluded the html using .gitignore

--- download-problem.sh
+++ download-problem.sh
@@ -1,21 +1,63 @@
 #!/bin/bash
 
-if [ "$#" -lt "2" ]; then
-    echo "usage: $0 <contest> <problem>" >&2
+if [ "$#" -lt "1" ]; then
+    echo "usage: $0 <problem-code> ..." >&2
     echo "e.g.: $0 460 D" >&2
     exit 1
 fi
 
-dir=problems/$1$2
-index=$dir/_$1$2.html
-curd=$(pwd)
+for prob in $@; do
+    p_num=${prob:0:3}
+    p_let=${prob:3:1}
+    dir=./problems
+    dl_dir=$dir/$prob
+    index=$dl_dir/_$prob.html
+    pdf=$dir/$prob.pdf
+    pkg=$dir/$prob.html.tar.gz
+    curd=$(pwd)
 
-if [ ! -e $index ]; then
-    mkdir -p $dir
-    wget http://codeforces.com/problemset/problem/$1/$2 -kp -H -nd -e robots=off -np -A jpg,jpeg,png,svg,gif,css, -P $dir >&2
-    mv $dir/$2 $index
-fi
+    if [ ! -e $index ] && [ ! -e $pdf ] && [ ! -e $pkg ]; then
+        mkdir -p $dir
+        echo "$" wget http://codeforces.com/problemset/problem/$p_num/$p_let "..."
+        wget http://codeforces.com/problemset/problem/$p_num/$p_let \
+            -kp -H -nd -e robots=off -np -A jpg,jpeg,png,svg,gif,css,$p_let -P $dl_dir -q
+        mv $dl_dir/$p_let $index
+        echo "created:"
+        echo "  file://$curd/$index"
+        echo
+        echo "$" du -sh $dl_dir
+        du -sh $dl_dir
+    elif [ -e $index ]; then
+        echo "existing:"
+        echo "  file://$curd/$index"
+    fi
 
-echo
-echo "  file://$curd/$index"
-echo
+    which wkhtmltopdf &>/dev/null
+    ret=$?
+    if [ $ret -eq 0 ] && [ -e $index ] && [ ! -e $pdf ]; then
+        echo "$" wkhtmltopdf --allow $dir $index $pdf
+        wkhtmltopdf --allow $dir $index $pdf -q 
+        if [ $? -eq 0 ]; then
+            echo
+            echo "  file://$curd/$pdf"
+            echo "  $pdf"
+            echo
+        else
+            echo "Error creating pdf" >&2
+        fi
+    elif [ $ret -gt 0 ]; then
+        echo "Install wkhtmltopdf to crate PDF"
+        echo "  sudo apt-get install wkhtmltopdf"
+        echo ""
+        echo "  [ -f /usr/lib/libGL.so.1 ] || sudo" \
+            "ln -s /usr/lib/x86_64-linux-gnu/mesa/libGL.so.1 /usr/lib/libGL.so.1 "
+    fi
+
+    if [ -e $index ] && [ -e $pdf ]; then
+        echo "$" tar czf $pkg $dl_dir
+        tar czf $pkg $dl_dir \
+            && rm -rf $dl_dir
+        echo "  $pkg"
+        echo
+    fi
+done
