063e0d4852418f5c71c7783236556b5281fac809
Matej 'Yin' Gagyi <yinotaurus@gmail.com>
2014-08-30 01:51:38 +0200

    Solution-crawler and first download-problem script

---
+++ download-problem.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$#" -lt "2" ]; then
+    echo "usage: $0 <contest> <problem>" >&2
+    echo "e.g.: $0 460 D" >&2
+    exit 1
+fi
+
+dir=problems/$1$2
+index=$dir/_$1$2.html
+curd=$(pwd)
+
+if [ ! -e $index ]; then
+    mkdir -p $dir
+    echo wget http://codeforces.com/problemset/problem/$1/$2 -kp -H -nd -e robots=off -np -A jpg,jpeg,png,svg,gif,css, -P $dir
+    wget http://codeforces.com/problemset/problem/$1/$2 -kp -H -nd -e robots=off -np -A jpg,jpeg,png,svg,gif,css, -P $dir >&2
+    mv $dir/$2 $index
+fi
+
+echo
+echo "  file://$curd/$index"
+echo
