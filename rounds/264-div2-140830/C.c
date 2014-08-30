a29550c4a78dfea628d567177d424e0691002249
Matej 'Yin' Gagyi <yinotaurus@gmail.com>
2014-08-30 14:27:10 +0200

    Finished 463C under contest 264

--- rounds/264-div2-140830/C.c
+++ rounds/264-div2-140830/C.c
@@ -1,40 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 #include <limits.h>
 
+long long f[4001111];
+
 int main() {
-  long long n, h, l[4001], r[4001];
-  long long i, j;
-  scanf("%lld\n", &n);
-  for (i = 0; i < n; i++) {
-    for (j = 0; j < n; j++) {
-      scanf("%lld\n", &h);
-      int li = n + i - j;
-      int ri = i + j;
-      l[li] += h;
-      r[ri] += h;
+  long long l[4001], r[4001];
+  int n, x, y, h;
+  int q = 10;
+  scanf("%d\n", &n);
+  for (h = 0; h < 2*n+1; h++) r[h] = l[h] = 0;
+  for (y = 0; y < n; y++) {
+    for (x = 0; x < n; x++) {
+      h = y*n+x;
+      scanf("%lld\n", &f[h]);
+      int li = n + y - x;
+      int ri = y + x;
+      l[li] += f[h];
+      r[ri] += f[h];
     }
   }
-  long long max1 = 0, max2=0, max1_i=0, max1_j = 0, max2_i = 0, max2_j = 0;
-  for (i = 0; i < 2*n-1; i++) {
-    for (j = 0; j < 2*n-1; j++) {
-      long long s = l[i] + r[j];
-      if (max1 < s && (i-j)%2 == 0) {
+  long long max1 = -1, max2 = -1;
+  int max1_x = 0, max1_y = 0, max2_x = 0, max2_y = 0;
+  for (y = 0; y < n; y++) {
+    for (x = 0; x < n; x++) {
+      h = y*n+x;
+      int li = n + y - x;
+      int ri = y + x;
+      long long s = l[li] + r[ri] - f[h];
+      if (max1 < s && (y-x)%2 == 0) {
         max1 = s;
-        max1_i = i;
-        max1_j = j;
+        max1_x = x;
+        max1_y = y;
       }
-      if (max2 < s && (i-j)%2 == 1) {
+      if (max2 < s && abs(y-x)%2 == 1) {
         max2 = s;
-        max2_i = i;
-        max2_j = j;
+        max2_x = x;
+        max2_y = y;
       }
     }
   }
-  long long x1 = (n + max1_j - max1_i) / 2;
-  long long y1 = max1_j - x1;
-  long long x2 = (n + max2_j - max2_i) / 2;
-  long long y2 = max2_j - x2;
   printf("%lld\n", max1+max2);
-  printf("%lld %lld %lld %lld\n", y1+1, x1+1, y2+1, x2+1); // rows are x1, x2
+  printf("%d %d %d %d\n", max1_y+1, max1_x+1, max2_y+1, max2_x+1); // rows are x1, x2
   return 0;
 }
