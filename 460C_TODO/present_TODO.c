92fdb3e39df4b04b27871589476831c9ba494138
Matej 'Yin' Gagyi <yinotaurus@gmail.com>
2014-08-30 02:13:21 +0200

    Work on 460C and 461B

---
+++ 460C_TODO/present_TODO.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  quick();
+  return 0;
+}
+
+void proper() {
+  int num_flowers;
+  int *flower_h;
+  scanf("%d", &num_flowers);
+  for (int i = 0; i < num_lowers; i++) {
+    scanf("%d", &flower_h);
+  }
+  printf("%d\n", num_flowers);
+}
+
+
+void quick() {
+  int n, m, w; // num_flowers, days_left, watering_length
+  long *A;     // flower_height
+    scanf("%d %d %d", &n, &m, &w);
+  for (int i = 0; i < n; i++) {
+    scanf("%d", F + i);
+  }
+  
+}
