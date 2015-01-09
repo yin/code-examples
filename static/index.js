5d55365fabb2ea313ae01c471604313e0b936fec
Matej 'Yin' Gagyi <yinotaurus@gmail.com>
2015-01-09 20:08:48 +0100

    Separated Rubik cube model, renderer and low-level opengl. Scene management, like camera, is still in the renderer.

--- static/index.js
+++ static/index.js
@@ -1,35 +1,43 @@
 var main = function($) {
     var canvas;
     canvas = document.getElementById('canvas');
-    var c = cube(canvas);
-    var lx, ly, dx, dy;
+    var c = new Cube3x3();
+    c.initModel();
+    var cubegl = new CubeWebGL(canvas, c);
+    cubegl.init();
+    var sliding = false, lx, ly, dx, dy;
     var slide = function() {
-        if (dx>0.01||dy>0.01) {
+        if(arguments.length > 0) {
+            sliding = !!arguments[0];
+        }
+        if (sliding && (Math.abs(dx) > 0.01 || Math.abs(dy) > 0.01)) {
             dx *= .995;
             dy *= .995;
-            c.drag(dx/2500, dy/2500);
-            c.draw();
+            cubegl.drag(dx/2500, dy/2500);
+            cubegl.draw();
+            setTimeout(slide, 10);
         }
-        setTimeout(slide, 10);
     };
-    var letgo = function() {
-        $(this).unbind('mousemove');
-        slide();
-    };
-    canvas = $('#canvas');
-    canvas.mousedown(function(ev) {
+    var grab = function(ev) {
         lx = ev.pageX;
         ly = ev.pageY;
+        slide(false);
         $(this).mousemove(function(ev) {
             dx = ev.pageX - lx;
             dy = ev.pageY - ly;
             lx = ev.pageX;
             ly = ev.pageY;
-            c.drag(dx/250, dy/250);
-            c.draw();
+            cubegl.drag(dx/250, dy/250);
+            cubegl.draw();
         });
-    });
+    };
+    var letgo = function() {
+        $(this).unbind('mousemove');
+        slide(true);
+    };
+    canvas = $('#canvas');
+    canvas.mousedown(grab);
     canvas.mouseup(letgo);
     canvas.mouseout(letgo);
-    c.draw();
+    cubegl.draw();
 }
