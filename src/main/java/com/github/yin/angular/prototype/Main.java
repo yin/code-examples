5dc4f13f9f3b63099efc240060c77b62fdc44119
Matej 'Yin' Gagyi <yinotaurus@gmail.com>
2016-02-28 23:25:01 +0100

    Adding some lines back brings back working annotation support on the servelt mapped to /hello

--- src/main/java/com/github/yin/angular/prototype/Main.java
+++ src/main/java/com/github/yin/angular/prototype/Main.java
@@ -1,9 +1,12 @@
 package com.github.yin.angular.prototype;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.startup.Tomcat;
-
 import java.io.File;
+
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
 
 public class Main {
 
@@ -14,9 +17,16 @@ public class Main {
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(PORT);
 
-        Context ctx = tomcat.addWebapp("/", new File(SRC_WEBAPP).getAbsolutePath());
+        StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(SRC_WEBAPP).getAbsolutePath());
         System.out.println("Serving files from path: " + new File("./" + SRC_WEBAPP).getAbsolutePath());
-        System.out.println(ctx.getServletContext().toString());
+
+        // Declare an alternative location for your "WEB-INF/classes" dir
+        // Servlet 3.0 annotation will work
+        File additionWebInfClasses = new File("target/classes");
+        WebResourceRoot resources = new StandardRoot(ctx);
+        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes",
+                additionWebInfClasses.getAbsolutePath(), "/"));
+        ctx.setResources(resources);
 
         tomcat.start();
         tomcat.getServer().await();
