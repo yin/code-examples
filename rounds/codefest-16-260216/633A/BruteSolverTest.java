b253bd7a36d7843e89a29258e554fd3f9cc9da74
Matej 'Yin' Gagyi <yinotaurus@gmail.com>
2016-02-28 13:36:07 +0100

    Preparing for testing on codeforces

---
+++ rounds/codefest-16-260216/633A/BruteSolverTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by yin on 28.2.16.
+ */
+public class BruteSolverTest {
+
+    @org.junit.Test
+    public void testSolve() throws Exception {
+
+    }
+}
